ebd4ef59a0dc948027a20a9867bd8554d2505ab0
Dmitriy Pushkov <ezze@ezze.org>
2015-06-18 13:34:32 +0300

    package.json and Gruntfile.js are added.

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+/* global module: false, require: false, process: false */
+module.exports = function(grunt) {
+    'use strict';
+
+    var path = require('path');
+    require('load-grunt-config')(grunt, {
+        configPath: path.join(process.cwd(), 'grunt'),
+        init: true,
+        data: {
+            pkg: grunt.file.readJSON('package.json')
+        }
+    });
+
+    grunt.registerTask('build', [
+        'jshint'
+    ]);
+
+    grunt.registerTask('rebuild', [
+        'clean',
+        'build'
+    ]);
+
+    grunt.registerTask('default', [
+        'build'
+    ]);
+};
---
+++ grunt/jshint.js
@@ -0,0 +1,25 @@
+/* global module: false */
+module.exports = function(grunt) {
+    'use strict';
+
+    return {
+        options: {
+            jshintrc: '.jshintrc'
+        },
+        gruntfile: {
+            files: {
+                src: [
+                    'Gruntfile.js',
+                    'grunt/**/*.js'
+                ]
+            }
+        },
+        library: {
+            files: {
+                src: [
+                    'src/**/*.js'
+                ]
+            }
+        }
+    };
+};
