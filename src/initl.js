060b5796daba3267f1abc0edd6245c592ca7d48a
Dmitriy Pushkov <ezze@ezze.org>
2017-04-13 22:18:24 +0300

    ESLint errors in initl are fixed.

--- src/initl.js
+++ src/initl.js
@@ -52,54 +52,53 @@ import { twoPi, x2o3, xke, j2 } from './constants';
  *    hoots, schumacher and glover 2004
  *    vallado, crawford, hujsak, kelso  2006
  ----------------------------------------------------------------------------*/
-export default function initl(initlParameters) {
-  let ecco = initlParameters.ecco,
-    epoch = initlParameters.epoch,
-    inclo = initlParameters.inclo,
-    no = initlParameters.no,
-    method = initlParameters.method,
-    opsmode = initlParameters.opsmode;
+export default function initl(options) {
+  const {
+    ecco,
+    epoch,
+    inclo,
+    opsmode,
+  } = options;
 
-  let ak,
-    d1,
-    adel,
-    po,
-    gsto;
+  let {
+    no,
+  } = options;
 
   // sgp4fix use old way of finding gst
-  //  ----------------------- earth constants ----------------------
-  //  sgp4fix identify constants and allow alternate values
+  // ----------------------- earth constants ---------------------
+  // sgp4fix identify constants and allow alternate values
 
-  //  ------------- calculate auxillary epoch quantities ----------
+  // ------------- calculate auxillary epoch quantities ----------
   const eccsq = ecco * ecco;
   const omeosq = 1.0 - eccsq;
   const rteosq = Math.sqrt(omeosq);
   const cosio = Math.cos(inclo);
   const cosio2 = cosio * cosio;
 
-  //  ------------------ un-kozai the mean motion -----------------
-  ak = Math.pow(xke / no, x2o3);
-  d1 = 0.75 * j2 * (3.0 * cosio2 - 1.0) / (rteosq * omeosq);
+  // ------------------ un-kozai the mean motion -----------------
+  const ak = Math.pow(xke / no, x2o3);
+  const d1 = (0.75 * j2 * ((3.0 * cosio2) - 1.0)) / (rteosq * omeosq);
   let delPrime = d1 / (ak * ak);
-  adel = ak * (1.0 - delPrime * delPrime - delPrime *
-      (1.0 / 3.0 + 134.0 * delPrime * delPrime / 81.0));
+  const adel = ak * (1.0 - (delPrime * delPrime) -
+    (delPrime * ((1.0 / 3.0) + ((134.0 * delPrime * delPrime) / 81.0))));
   delPrime = d1 / (adel * adel);
   no /= (1.0 + delPrime);
 
   const ao = Math.pow(xke / no, x2o3);
   const sinio = Math.sin(inclo);
-  po = ao * omeosq;
-  const con42 = 1.0 - 5.0 * cosio2;
+  const po = ao * omeosq;
+  const con42 = 1.0 - (5.0 * cosio2);
   const con41 = -con42 - cosio2 - cosio2;
   const ainv = 1.0 / ao;
   const posq = po * po;
   const rp = ao * (1.0 - ecco);
-  method = 'n';
+  const method = 'n';
 
   //  sgp4fix modern approach to finding sidereal time
+  let gsto;
   if (opsmode === 'a') {
-      //  sgp4fix use old way of finding gst
-      //  count integer number of days from 0 jan 1970
+    //  sgp4fix use old way of finding gst
+    //  count integer number of days from 0 jan 1970
     const ts70 = epoch - 7305.0;
     const ds70 = Math.floor(ts70 + 1.0e-8);
     const tfrac = ts70 - ds70;
@@ -108,7 +107,7 @@ export default function initl(initlParameters) {
     const thgr70 = 1.7321343856509374;
     const fk5r = 5.07551419432269442e-15;
     const c1p2p = c1 + twoPi;
-    gsto = (thgr70 + c1 * ds70 + c1p2p * tfrac + ts70 * ts70 * fk5r) % twoPi;
+    gsto = (thgr70 + (c1 * ds70) + (c1p2p * tfrac) + (ts70 * ts70 * fk5r)) % twoPi;
     if (gsto < 0.0) {
       gsto += twoPi;
     }
