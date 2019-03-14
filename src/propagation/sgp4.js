75787378b004444877eeeb36631f07bae354447a
Dmitriy Pushkov <ezze@ezze.org>
2019-03-14 11:03:40 +0300

    Reduce unnecessary calculations in sgp4.

--- src/propagation/sgp4.js
+++ src/propagation/sgp4.js
@@ -382,6 +382,17 @@ export default function sgp4(satrec, tsince) {
 
   const mrt = (rl * (1.0 - (1.5 * temp2 * betal * satrec.con41)))
     + (0.5 * temp1 * satrec.x1mth2 * cos2u);
+
+  // sgp4fix for decaying satellites
+  if (mrt < 1.0) {
+    // printf("// decay condition %11.6f \n",mrt);
+    satrec.error = 6;
+    return {
+      position: false,
+      velocity: false,
+    };
+  }
+
   su -= 0.25 * temp2 * satrec.x7thm1 * sin2u;
   const xnode = nodep + (1.5 * temp2 * cosip * sin2u);
   const xinc = xincp + (1.5 * temp2 * cosip * sinip * cos2u);
@@ -415,16 +426,6 @@ export default function sgp4(satrec, tsince) {
     y: ((mvt * uy) + (rvdot * vy)) * vkmpersec,
     z: ((mvt * uz) + (rvdot * vz)) * vkmpersec,
   };
-
-  // sgp4fix for decaying satellites
-  if (mrt < 1.0) {
-    // printf("// decay condition %11.6f \n",mrt);
-    satrec.error = 6;
-    return {
-      position: false,
-      velocity: false,
-    };
-  }
 
   return {
     position: r,
