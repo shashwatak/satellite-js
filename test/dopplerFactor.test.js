6c6d1c4fac52a0d0da2311ac4cc6af0b575a9c66
Alesha72003 <Alesha72003@ya.ru>
2020-12-18 00:10:00 +0300

    Correct programming style

--- test/dopplerFactor.test.js
+++ test/dopplerFactor.test.js
@@ -1,6 +1,7 @@
 import dopplerFactor from '../src/dopplerFactor'; // eslint-disable-line
 
 const earthRadius = 6378.137;
+const sincos45deg = Math.sqrt(2) / 2;
 
 describe('Doppler factor', () => {
   it('without doppler factor', () => {
@@ -8,40 +9,40 @@ describe('Doppler factor', () => {
     const observerEcf = {
       x: 0,
       y: 0,
-      z: earthRadius
+      z: earthRadius,
     };
     const positionEcf = {
       x: 0,
       y: 0,
-      z: earthRadius + 500
+      z: earthRadius + 500,
     };
-    //Escape velocity
+    // Escape velocity
     const velocityEcf = {
       x: 7.91,
       y: 0,
-      z: 0
+      z: 0,
     };
     const dopFactor = dopplerFactor(observerEcf, positionEcf, velocityEcf);
-    expect(dopFactor).toEqual(1)
+    expect(dopFactor).toEqual(1);
   });
-  
-  it("special case", () => {
+
+  it('special case', () => {
     const observerEcf = {
       x: earthRadius,
       y: 0,
-      z: 0
+      z: 0,
     };
     const positionEcf = {
-      x: (earthRadius + 500) * Math.sqrt(2) / 2, // z*sin(45)
-      y: (earthRadius + 500) * Math.sqrt(2) / 2, // z*cos(45)
-      z: 0
+      x: (earthRadius + 500) * sincos45deg, // z*sin(45)
+      y: (earthRadius + 500) * sincos45deg, // z*cos(45)
+      z: 0,
     };
     const velocityEcf = {
-      x: 7.91 * Math.sqrt(2) / 2,
-      y: 7.91 * Math.sqrt(2) / 2,
-      z: 0
+      x: 7.91 * sincos45deg,
+      y: 7.91 * sincos45deg,
+      z: 0,
     };
     const dopFactor = dopplerFactor(observerEcf, positionEcf, velocityEcf);
-    expect(dopFactor).toEqual(1.0000107847789212)
+    expect(dopFactor).toEqual(1.0000107847789212);
   });
 });
