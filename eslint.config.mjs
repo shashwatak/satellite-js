dfcf9205e6375b0add8a3e3b3036c21b291481df
Vladyslav Huba <vladhuba@gmail.com>
2025-03-30 14:51:16 +0300

    add typescript config, packages and linting

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    ignores: ['/dist', '/lib', '*.mjs']
+  }
+);
