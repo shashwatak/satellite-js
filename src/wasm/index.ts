03deec6b5ca6f49499e6c6d8a6cf6f1c8a87245b
Vladyslav Huba <vladhuba@gmail.com>
2025-09-09 16:50:22 +0300

    export createWasmModule

--- src/wasm/index.ts
+++ src/wasm/index.ts
@@ -1,3 +1,4 @@
+export { default as createWasmModule } from '../../wasm-build/release/index.js'
 export { BulkPropagator } from './bulk-propagator.js'
 
 export * from './calculators/calculators.js'
