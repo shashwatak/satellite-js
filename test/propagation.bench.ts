ea5a12f7121f75a0bce206d15ec058400428bc80
Vladyslav Huba <vladhuba@gmail.com>
2026-01-16 12:13:57 +0200

    include multithread propagator to bench and catalog test

--- test/propagation.bench.ts
+++ test/propagation.bench.ts
@@ -1,27 +1,41 @@
 import { bench } from 'vitest';
-import WasmModuleFactory from 'wasm-module/index.js';
+import createSingleThreadModule from 'wasm-module-single-thread/index.js';
+import createMultiThreadModule from 'wasm-module-multi-thread/index.js';
+import { createSingleThreadRuntimeFromModule } from '../src/wasm/runtimes/single-thread-runtime.js';
+import { createMultiThreadRuntimeFromModule } from '../src/wasm/runtimes/multi-thread-runtime.js';
 import { BulkPropagator, EciBaseCalculator } from '../src/wasm/index.js';
 import { json2satrec, propagate, SatRecError } from '../src/index.js';
 import type { OMMJsonObject } from '../src/common-types.js';
 import ommData from './omm.json' with { type: 'json' };
 
-const MAX_SATELLITES = 20000;
+const singleThreadRuntime = await createSingleThreadRuntimeFromModule(
+  await createSingleThreadModule(),
+);
+const multiThreadRuntime = await createMultiThreadRuntimeFromModule(
+  await createMultiThreadModule(),
+  { threadsCount: 4 },
+);
+
 const DATES_COUNT = 1;
 const DATE_START = new Date('2025-07-12T00:00:00.123Z');
 const DATE_STEP_MS = 60 * 60 * 1000;
 
 const satrecs = (ommData as OMMJsonObject[])
-  .slice(0, MAX_SATELLITES)
   .map((obj) => json2satrec(obj));
 const dates = Array.from(
   { length: DATES_COUNT },
   (_, i) => new Date(DATE_START.getTime() + i * DATE_STEP_MS),
 );
 
-const wasmModule = await WasmModuleFactory();
 // `using` here throws because benchmarks are executed separately after bp is disposed
 const bp = new BulkPropagator({
-  wasmModule,
+  runtime: singleThreadRuntime,
+  calculators: [new EciBaseCalculator()],
+  satRecs: satrecs,
+  datesCount: dates.length,
+});
+const multiThreadBp = new BulkPropagator({
+  runtime: multiThreadRuntime,
   calculators: [new EciBaseCalculator()],
   satRecs: satrecs,
   datesCount: dates.length,
@@ -29,8 +43,22 @@ const bp = new BulkPropagator({
 
 let sideEffectSink = 0; // avoid elimination of loops
 
-bench('WASM BulkPropagator', () => {
+bench('WASM Single-Thread BulkPropagator', () => {
   bp.run({ dates });
+  let local = 0;
+  for (let si = 0; si < satrecs.length; si++) {
+    for (let di = 0; di < dates.length; di++) {
+      const out = bp.getFormattedOutput(si, di)!.eci;
+      if (out.error === SatRecError.None) {
+        local += out.position.x + out.velocity.y;
+      }
+    }
+  }
+  sideEffectSink = local;
+});
+
+bench('WASM Multi-Thread BulkPropagator', async () => {
+  await multiThreadBp.run({ dates });
   let local = 0;
   for (let si = 0; si < satrecs.length; si++) {
     for (let di = 0; di < dates.length; di++) {
