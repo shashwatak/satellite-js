28119a3726bfa1c50446cdc96a98f56b95b305b5
Vladyslav Huba <vladhuba@gmail.com>
2026-01-16 17:15:32 +0200

    split sets for benchmarking to ECI and LookAngles

--- test/propagation.bench.ts
+++ test/propagation.bench.ts
@@ -1,10 +1,14 @@
-import { bench } from 'vitest';
+import { bench, describe } from 'vitest';
 import createSingleThreadModule from 'wasm-module-single-thread/index.js';
 import createMultiThreadModule from 'wasm-module-multi-thread/index.js';
 import { createSingleThreadRuntimeFromModule } from '../src/wasm/runtimes/single-thread-runtime.js';
 import { createMultiThreadRuntimeFromModule } from '../src/wasm/runtimes/multi-thread-runtime.js';
-import { BulkPropagator, EciBaseCalculator } from '../src/wasm/index.js';
-import { json2satrec, propagate, SatRecError } from '../src/index.js';
+import {
+  BulkPropagator, EcfPositionCalculator, EciBaseCalculator, GmstCalculator, LookAnglesCalculator,
+} from '../src/wasm/index.js';
+import {
+  ecfToLookAngles, eciToEcf, gstime, json2satrec, propagate, SatRecError,
+} from '../src/index.js';
 import type { OMMJsonObject } from '../src/common-types.js';
 import ommData from './omm.json' with { type: 'json' };
 
@@ -16,7 +20,7 @@ const multiThreadRuntime = await createMultiThreadRuntimeFromModule(
   { threadsCount: 4 },
 );
 
-const DATES_COUNT = 1;
+const DATES_COUNT = 24 * 365;
 const DATE_START = new Date('2025-07-12T00:00:00.123Z');
 const DATE_STEP_MS = 60 * 60 * 1000;
 
@@ -28,60 +32,136 @@ const dates = Array.from(
 );
 
 // `using` here throws because benchmarks are executed separately after bp is disposed
-const bp = new BulkPropagator({
-  runtime: singleThreadRuntime,
-  calculators: [new EciBaseCalculator()],
-  satRecs: satrecs,
-  datesCount: dates.length,
-});
-const multiThreadBp = new BulkPropagator({
-  runtime: multiThreadRuntime,
-  calculators: [new EciBaseCalculator()],
-  satRecs: satrecs,
-  datesCount: dates.length,
-});
 
 let sideEffectSink = 0; // avoid elimination of loops
 
-bench('WASM Single-Thread BulkPropagator', () => {
-  bp.run({ dates });
-  let local = 0;
-  for (let si = 0; si < satrecs.length; si++) {
-    for (let di = 0; di < dates.length; di++) {
-      const out = bp.getFormattedOutput(si, di)!.eci;
-      if (out.error === SatRecError.None) {
-        local += out.position.x + out.velocity.y;
+const iterationSettings = { warmupIterations: 10, iterations: 20 };
+
+describe('ECI', () => {
+  const bp = new BulkPropagator({
+    runtime: singleThreadRuntime,
+    calculators: [new EciBaseCalculator()],
+    satRecs: satrecs,
+    datesCount: dates.length,
+  });
+  const multiThreadBp = new BulkPropagator({
+    runtime: multiThreadRuntime,
+    calculators: [new EciBaseCalculator()],
+    satRecs: satrecs,
+    datesCount: dates.length,
+  });
+
+  bench('WASM Single-Thread BulkPropagator', () => {
+    bp.run({ dates });
+    let local = 0;
+    for (let si = 0; si < satrecs.length; si++) {
+      for (let di = 0; di < dates.length; di++) {
+        const out = bp.getFormattedOutput(si, di)!.eci;
+        if (out.error === SatRecError.None) {
+          local += out.position.x + out.velocity.y;
+        }
       }
     }
-  }
-  sideEffectSink = local;
+    sideEffectSink = local;
+  }, iterationSettings);
+
+  bench('WASM Multi-Thread BulkPropagator', async () => {
+    await multiThreadBp.run({ dates });
+    let local = 0;
+    for (let si = 0; si < satrecs.length; si++) {
+      for (let di = 0; di < dates.length; di++) {
+        const out = bp.getFormattedOutput(si, di)!.eci;
+        if (out.error === SatRecError.None) {
+          local += out.position.x + out.velocity.y;
+        }
+      }
+    }
+    sideEffectSink = local;
+  }, iterationSettings);
+
+  bench('Pure JS propagate loop', () => {
+    let local = 0;
+    for (const date of dates) {
+      for (const satrec of satrecs) {
+        const pv = propagate(satrec, date);
+        if (pv) {
+          local += pv.position.x + pv.velocity.y;
+        }
+      }
+    }
+    sideEffectSink = local;
+  }, iterationSettings);
 });
 
-bench('WASM Multi-Thread BulkPropagator', async () => {
-  await multiThreadBp.run({ dates });
-  let local = 0;
-  for (let si = 0; si < satrecs.length; si++) {
-    for (let di = 0; di < dates.length; di++) {
-      const out = bp.getFormattedOutput(si, di)!.eci;
-      if (out.error === SatRecError.None) {
-        local += out.position.x + out.velocity.y;
-      }
-    }
-  }
-  sideEffectSink = local;
-});
+describe('LookAngles', () => {
+  const bp = new BulkPropagator({
+    runtime: singleThreadRuntime,
+    calculators: [
+      new EciBaseCalculator(),
+      new GmstCalculator(),
+      new EcfPositionCalculator(),
+      new LookAnglesCalculator(),
+    ],
+    satRecs: satrecs,
+    datesCount: dates.length,
+  });
+  const multiThreadBp = new BulkPropagator({
+    runtime: multiThreadRuntime,
+    calculators: [
+      new EciBaseCalculator(),
+      new GmstCalculator(),
+      new EcfPositionCalculator(),
+      new LookAnglesCalculator(),
+    ],
+    satRecs: satrecs,
+    datesCount: dates.length,
+  });
 
-bench('Pure JS propagate loop', () => {
-  let local = 0;
-  for (const date of dates) {
-    for (const satrec of satrecs) {
-      const pv = propagate(satrec, date);
-      if (pv) {
-        local += pv.position.x + pv.velocity.y;
+  bench('WASM LookAngles Single-Thread BulkPropagator', () => {
+    bp.run({ dates, lookAngles: { observer: { longitude: 0, latitude: 0, height: 0 } } });
+    let local = 0;
+    for (let si = 0; si < satrecs.length; si++) {
+      for (let di = 0; di < dates.length; di++) {
+        const out = bp.getFormattedOutput(si, di);
+        if (out!.eci.error === SatRecError.None) {
+          local += out!.lookAngles.azimuth + out!.lookAngles.elevation;
+        }
       }
     }
-  }
-  sideEffectSink = local;
+    sideEffectSink = local;
+  }, iterationSettings);
+
+  bench('WASM LookAngles Multi-Thread BulkPropagator', async () => {
+    await multiThreadBp.run(
+      { dates, lookAngles: { observer: { longitude: 0, latitude: 0, height: 0 } } },
+    );
+    let local = 0;
+    for (let si = 0; si < satrecs.length; si++) {
+      for (let di = 0; di < dates.length; di++) {
+        const out = multiThreadBp.getFormattedOutput(si, di);
+        if (out!.eci.error === SatRecError.None) {
+          local += out!.lookAngles.azimuth + out!.lookAngles.elevation;
+        }
+      }
+    }
+    sideEffectSink = local;
+  }, iterationSettings);
+
+  bench('LookAngles Pure JS propagate loop', () => {
+    let local = 0;
+    for (const date of dates) {
+      const gmst = gstime(date);
+      for (const satrec of satrecs) {
+        const pv = propagate(satrec, date);
+        if (pv) {
+          const positionEcf = eciToEcf(pv.position, gmst);
+          const lookAngles = ecfToLookAngles({ longitude: 0, latitude: 0, height: 0 }, positionEcf);
+          local += lookAngles.azimuth + lookAngles.elevation;
+        }
+      }
+    }
+    sideEffectSink = local;
+  }, iterationSettings);
 });
 
 export const benchmarkGuard = () => sideEffectSink;
