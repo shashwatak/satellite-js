19fcfeacf4783d70691bbe5d331de7b705cbcf45
Vladyslav Huba <vladhuba@gmail.com>
2026-03-11 15:41:07 +0200

    add trailingSlash: false as recommended for gh-pages

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -24,6 +24,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'shashwatak', // Usually your GitHub org/user name.
   projectName: 'satellite-js', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
