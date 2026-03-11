4d7a4566c68a1f8ca4b958695bec2dd83d4990be
Vladyslav Huba <vladhuba@gmail.com>
2026-03-11 15:46:17 +0200

    remove sgp4_verification

--- sgp4_verification/background.js
+++
@@ -1,5 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function(launchData) {
-  chrome.app.window.create('index.html', {width: 1400, height: 850}, function(win) {
-    win.contentWindow.launchData = launchData;
-  });
-});
