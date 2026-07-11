3a40dcbbdd890eeaadbab419d687db55ed5c82f8
Tyler Hughes <tylerhx111+git@proton.me>
2026-07-11 15:53:12 -0400

    added unit tests for checking mean motion w/ and w/o kozai

--- test/io.test.ts
+++ test/io.test.ts
@@ -66,3 +66,14 @@ describe('twoline2satrec', () => {
     expect(satrec.ecco).toBeCloseTo(0.0000123, 10);
   });
 });
+
+describe('mean motion', () => {
+  it('should be stored with and without kozai', () => {
+    const tle1 = `1 99999U 25999A   25274.00000000 -.00000000  00000-0  00000-0 0    14`;
+    const tle2 = `2 99999  50.0000 142.8988     123 180.0001 210.9293 14.73473854000071`;
+    const satrec = twoline2satrec(tle1, tle2);
+    expect(satrec.no).toBeCloseTo(0.06428212791307905, 10);
+    expect(satrec.nokozai).toBeCloseTo(0.06429242548587555, 10);
+    expect(satrec.nokozai * rad2deg * 4).toBeCloseTo(14.73473854, 10);
+  });
+});
